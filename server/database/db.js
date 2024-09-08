4901ac6ca85cdad1759545be2e9c15bff18ad303
Adarsh Kumbar <adarshkumbar0710@gmail.com>
2024-09-08 19:20:50 +0530

    update

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@freshfeed.rvfm6.mongodb.net/`;
+    const URL = `mongodb+srv://${username}:${password}@freshfeed.rvfm6.mongodb.net/Project 0?retryWrites=true&w=majority`;
     try {
         await mongoose.connect(URL, {useNewUrlParser : true});
         console.log('Database connected successfully')
