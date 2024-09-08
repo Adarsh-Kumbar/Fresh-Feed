a7745cfe87389bdacd611334087b16daf345eabb
Adarsh Kumbar <adarshkumbar0710@gmail.com>
2024-09-08 19:31:47 +0530

    updated

--- server/database/db.js
+++ server/database/db.js
@@ -5,10 +5,14 @@ import mongoose from "mongoose"
 const Connection = async (username, password) => {
     const URL = `mongodb+srv://${username}:${password}@freshfeed.rvfm6.mongodb.net/Project 0?retryWrites=true&w=majority`;
     try {
-        await mongoose.connect(URL, {useNewUrlParser : true});
-        console.log('Database connected successfully')
+        await mongoose.connect(URL, { 
+            useUnifiedTopology: true, 
+            useNewUrlParser: true, 
+            serverSelectionTimeoutMS: 30000 // 30 seconds
+        });
+        console.log('Database connected successfully');
     } catch (error) {
-        console.log('error while connecting with the database', error)
+        console.log('Error while connecting with the database', error);
     }
 
 }
